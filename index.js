acf3fc98eb0bb1096ba893dbfd1ab58c6f8ea577
sheikhilyas <sheikhilyas031631@gmail.comgit config --global user.email sheikhilyas031631@gmail.com>
2024-04-16 00:49:26 +0500

    Addind Simple Calculator Exercises

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+const answer = await inquirer.prompt([
+    { message: 'Enter first number', type: 'number', name: 'firstNumber' },
+    { message: 'Enter second number', type: 'number', name: 'secondNumber' },
+    {
+        message: 'Select one of the operator to perform action',
+        type: 'list',
+        name: 'operation',
+        choices: ['Addition', 'Subtraction', 'Multiplication', 'Division'],
+    },
+]);
+// conditional statement
+if (answer.operation === 'Addition') {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operation === 'Subtraction') {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operation === 'Multiplication') {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operation === 'Division') {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Pleace Select Valid Operaror");
+}
